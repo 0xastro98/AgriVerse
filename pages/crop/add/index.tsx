ea53bb66c312091b4e995ffe967b3d411e721c12
Amrit Shenava <11773594+0xastro98@users.noreply.github.com>
2025-05-16 17:25:20 +0000

    update

--- pages/crop/add/index.tsx
+++ pages/crop/add/index.tsx
@@ -1,8 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import {useRouter} from 'next/router'
-import styles from '../../../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../../styles/Home.module.css";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {Program, Idl, AnchorProvider, setProvider} from '@coral-xyz/anchor';
+import { useAnchorWallet, useConnection, useWallet} from "@solana/wallet-adapter-react";
+import {usePrivy, useLoginWithEmail, useSolanaWallets} from '@privy-io/react-auth'
+import idl from '../../../idl/idl.json'
+import {AgriVerse} from '../../../types/idl'
+import { Keypair, PublicKey, VersionedTransaction, Transaction, sendAndConfirmTransaction, Connection} from "@solana/web3.js";
 import {Amaranth} from 'next/font/google'
 import {Form, Button} from 'react-bootstrap'
+import { Router } from "next/router";
 
 const amaranth = Amaranth({subsets: ['latin'], weight: '400'})
 
@@ -11,8 +21,14 @@ export default function Add(){
   const [cropName, setCropName] = useState()
   const [cropType, setCropType] = useState()
 
+  /*
   const addCropFunc = () => {
+    const transaction = await program.methods
+    .addCrop(cropName, cropType)
+    .accounts({cropInfo: publicKey, systemProgram: programId})
+    .rpc();
   }
+  */
   return(
     <>
       <center>
@@ -25,8 +41,15 @@ export default function Add(){
               </Form.Group>
               <Form.Group className='mb-4'>
                 <Form.Select defaultValue='Select Crop Type'>
-                  <
+                  <option disabled value='Select Crop Type'>Select Crop Type</option>
+                  <option value='Fruit'>Fruit</option>
+                  <option value='Vegetable'>Vegetable</option>
                 </Form.Select>
+              </Form.Group>
+              <Form.Group className='mb-4'>
+                <div className={styles.avbtn}>
+                  <Button variant="custom">Add</Button>
+                </div>
               </Form.Group>
             </Form>
           </div>
